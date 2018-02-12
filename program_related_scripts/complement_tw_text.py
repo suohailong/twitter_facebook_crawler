66c0e77c0bb97a04d65024e1e8d2337b5d9cd858
suo <250313341@qq.com>
2018-02-12 09:47:38 +0800

    补充

--- program_related_scripts/complement_tw_text.py
+++ program_related_scripts/complement_tw_text.py
@@ -12,6 +12,8 @@ from aiohttp import ClientSession
 import aiohttp,os,sys
 import asyncio
 import multiprocessing as mp
+sys.path.append(".")
+from src.redis_helper import RedisQueue
 
 
 
@@ -50,7 +52,9 @@ def asynchronous_request(ops=[]):
                     print(cun_result.text)
                     return cun_result.text
             except Exception as e:
-               raise e
+               print('访问出了错误')
+               print(e)
+               return json.loads({"success" : "false"})
 
     asyncio.set_event_loop(asyncio.new_event_loop())
     loop = asyncio.get_event_loop()
@@ -62,12 +66,21 @@ def asynchronous_request(ops=[]):
     return [json.loads(task.result()) for task in tasks]
 
 def completion_twitter_text(conn):
-    with open('twitter_user_ids.json','r') as f:
-        ids = json.load(f)
+    # with open('twitter_user_ids.json','r') as f:
+    #     ids = json.load(f)
     current = 0;
-    for id in ids['ids']:
+    # for id in ids['ids']:
+    with open(os.path.abspath('config.json'), 'r') as f:
+        app_config = json.load(f)
+    twitter_crawler_queue = RedisQueue(name='twitter_test_ids', redis_config=app_config['redis_config2'])
+    while True:
         try:
             # print(id)
+            if twitter_crawler_queue.empty():
+                print('所有的用户全部跑完')
+                break
+            id = twitter_crawler_queue.get()
+            print('[===取出的ID为%s,目前还有%s条需要抓取====]' % (id,twitter_crawler_queue.qsize()))
             es_url = 'http://narnia.idatage.com/stq/api/v1/rowlet/findEsTextByUserIdOrKeywords?startDate=2016-12-30&endDate=2018-02-12&category=tw&ids=%s' % (id,)  # if i in [1,2,3,4,5,6,7,8] else 'http://narnia.idatage.com/stq/api/v1/rowlet/findEsTextByUserIdOrKeywords?startDate=2017-%s-01&endDate=2017-%s-01&category=%s&ids=%s' % (i,i+1,category,id)
             es_body = requests.get(es_url)
             # print(es_body.text)
@@ -82,28 +95,26 @@ def completion_twitter_text(conn):
                 return x['_source'];
             es_body_tw_urls = list(map(handele,filter(lambda x:not x['_source']['truncated'],es_body_tw)))
             # print(es_body_tw_urls)
-            if len(es_body_tw_urls)>50:
+            if len(es_body_tw_urls)>200:
                 pool = mp.Pool()
-                res = pool.map(asynchronous_request,(es_body_tw_urls[i:i+50] for i in range(0,len(es_body_tw_urls),50)))
+                res = pool.map(asynchronous_request,(es_body_tw_urls[i:i+200] for i in range(0,len(es_body_tw_urls),200)))
                 # current += 1;
-                print('更新%用户' % id)
+                print('更新%s用户' % id)
             elif 0<len(es_body_tw_urls)<50:
                 asynchronous_request(ops=es_body_tw_urls)
                 # current += 1;
-                print('更新%用户' % id)
+                print('更新%s用户' % id)
                 # print('第几%s个' % current)
             else:
                 current += 1;
                 print('该用户%s无需更新' % id)
                 print('第几%s个' % current)
-                if current == len(ids['ids']):
-                    break;
             conn.send(id)
         except Exception as e:
             current = 0
-            print(e)
-            # raise e
-            continue
+            # print(e)
+            raise e
+            # continue
 
 
 
@@ -121,7 +132,7 @@ if __name__== '__main__':
     p1.start()
     while True:
         if p1.is_alive():
-            if parent_conn.poll(60):
+            if parent_conn.poll(200):
                 print('接收到消息')
                 print('%s用户已更新完毕' % parent_conn.recv())
             else:
